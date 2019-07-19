6510f5a5fc7a88a36d94f84d95b4484e82b8228d
Alex Rodin <alex.rodin@sas.com>
2019-07-19 08:47:42 -0400

    The latest version from a local GitLab
    The latest version from a local GitLab

--- lasraction.py
+++ lasraction.py
@@ -3,9 +3,20 @@ import io
 import os
 import sys
 
-if sys.version_info[0] < 3:
-    print("Error: Your Python interpreter must be version 3 or greater")
-    exit()
+
+def check_installation(version):
+    current_version = sys.version_info
+    if current_version[0] == version[0] and current_version[1] >= version[1]:
+        pass
+    else:
+        sys.stderr.write(
+            "[%s] - Error: Your Python interpreter must be %d.%d or greater (within major version %d)\n" % (
+                sys.argv[0], version[0], version[1], version[0]))
+        sys.exit(-1)
+    return 0
+
+
+check_installation((3, 0))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=str, help="")
@@ -21,11 +32,14 @@ def main():
             for line in f:
                 for item in line.split(','):
                     attr = item.split('=')
-
-                    if attr[0] == 'ID' and int(attr[1]) == args.id:
-                        print_line = True
-                    elif attr[0] == 'ID' and int(attr[1]) != args.id:
-                        print_line = False
+                    
+                    try:
+                        if attr[0] == 'ID' and int(attr[1]) == args.id:
+                            print_line = True
+                        elif attr[0] == 'ID' and int(attr[1]) != args.id:
+                            print_line = False
+                    except:
+                        pass
 
                 if print_line:
                     print(line, end='')
